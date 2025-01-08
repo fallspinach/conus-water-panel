60acc3d26ac0bee32ac1bcbc77f0279b2eeebe13
Ming Pan <m3pan@ucsd.edu>
2025-01-07 19:16:21 -0800

    First commit for the CONUS web app.

---
+++ callbacks.py
@@ -0,0 +1,104 @@
+from main import app
+from config import all_stations, fnf_stations
+
+from dash.dependencies import ClientsideFunction, Input, Output, State
+from datetime import datetime, timedelta
+import pandas as pd
+
+from site_tools import draw_retro, draw_mofor, draw_table, draw_table_all
+from basin_tools import draw_basin_ts
+from snow_tools import draw_course, draw_pillow
+from river_tools import draw_mofor_river_db, draw_rev_esp
+from config import base_url
+## Callbacks from here on
+
+# callback to update data var in the title section
+app.clientside_callback(
+    ClientsideFunction(
+        namespace='clientside',
+        function_name='update_title_var'
+    ),
+    Output('title-var', 'children'),
+    Input(component_id='data-sel',  component_property='value'),
+    Input(component_id='met-vars',  component_property='value'),
+    Input(component_id='hydro-vars', component_property='value')
+)
+
+# callback to update data date in the title section
+app.clientside_callback(
+    ClientsideFunction(
+        namespace='clientside',
+        function_name='update_title_date'
+    ),
+    Output('title-date', 'children'),
+    Input('datepicker', 'date')
+)
+
+# callback to update url of image overlay
+app.clientside_callback(
+    ClientsideFunction(
+        namespace='clientside',
+        function_name='update_img_url'
+    ),
+    Output('data-img', 'url'),
+    Input('datepicker', 'date'),
+    Input(component_id='data-sel',  component_property='value'),
+    Input(component_id='met-vars',  component_property='value'),
+    Input(component_id='hydro-vars', component_property='value')
+)
+
+# callback to update url of color bar
+app.clientside_callback(
+    ClientsideFunction(
+        namespace='clientside',
+        function_name='update_cbar'
+    ),
+    Output('data-cbar-img', 'src'),
+    Input(component_id='data-sel',  component_property='value'),
+    Input(component_id='met-vars',  component_property='value'),
+    Input(component_id='hydro-vars', component_property='value')
+)
+
+app.clientside_callback(
+    ClientsideFunction(
+        namespace='clientside',
+        function_name='update_cbar_visibility'
+    ),
+    Output('data-cbar', 'style'),
+    Input(component_id='data-map-ol', component_property='checked')
+)
+
+# callback to update datepicker and slider on button clicks
+app.clientside_callback(
+    ClientsideFunction(
+        namespace='clientside',
+        function_name='update_date'
+    ),
+    Output('datepicker', 'date'),
+    Input('button-forward-day',  'n_clicks_timestamp'),
+    Input('button-backward-day', 'n_clicks_timestamp'),
+    Input('button-forward-month',   'n_clicks_timestamp'),
+    Input('button-backward-month',  'n_clicks_timestamp'),
+    Input('datepicker', 'date'),
+    Input('datepicker', 'min_date_allowed'),
+    Input('datepicker', 'max_date_allowed')
+)
+
+# update system status periodically
+@app.callback(Output(component_id='datepicker', component_property='max_date_allowed'),
+              Input('interval-check_system', 'n_intervals'))
+def update_system_status(basin):
+    df_status = pd.read_csv(f'{base_url}/data/system_status.csv', parse_dates=True)
+    return datetime.fromisoformat(df_status['WRF-Hydro NRT'][1]).date()
+
+# callback to switch HUC sources according to zoom level
+app.clientside_callback(
+    ClientsideFunction(
+        namespace='clientside',
+        function_name='switch_huc'
+    ),
+    Output('huc-bound', 'url'),
+    Output('huc-bound', 'zoomToBoundsOnClick'),
+    Input('map-region', 'zoom')
+)
+
